e8c22f5ce49d617bde5dbdcddb404dec881ec420
Patient-Roy <roy-mbaya@mail.ru>
2025-05-23 15:41:31 +0500

    unit22

--- docs/index.js
+++ docs/index.js
@@ -369,7 +369,8 @@ function updateOpeningHours() {
     if (timeEl) {
         timeEl.textContent = now.toLocaleTimeString([], {
             hour: '2-digit',
-            minute: '2-digit'
+            minute: '2-digit',
+            second: '2-digit'
         });
     }
 
